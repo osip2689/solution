701c0627c8099772b24d474c44cf60745500f1b4
osip2689 <osip2689@yandex.ru>
2016-11-17 15:11:09 +0300

    test

---
+++ Test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Андрей on 17.11.2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
