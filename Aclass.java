08945b1a1d0f72a30f4a77e74b72490a97cf7596
osip2689 <osip2689@yandex.ru>
2017-03-30 11:13:18 +0300

    test

---
+++ Aclass.java
@@ -0,0 +1,7 @@
+class Aclass{
+	private String name;
+	
+	public Aclass(){
+		
+	}
+}
--- Test.java
+++ Test.java
@@ -6,5 +6,6 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+		System.out.println("Again hello, world!");
     }
 }
